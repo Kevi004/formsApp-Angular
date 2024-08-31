25f0006f5bf71e4e72f5eb974bf540ae48bb3c3c
Kevin Carrasco <kepler401@gmail.com>
2024-08-30 18:45:14 -0600

    Primeras validaciones agregadas

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -8,21 +8,22 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 export class BasicPageComponent {
 
   // public myForm: FormGroup = new FormGroup({
-  //   name:       new FormControl(''),
+  //   name:       new FormControl('',[]),
   //   price:      new FormControl(0),
   //   inStorage:  new FormControl(0),
   // });
   public myForm: FormGroup = this.fb.group({
-    name:[''],
-    price:[0],
-    inStorage:[0],
+    name:['',     [ Validators.required, Validators.minLength(3) ]],
+    price:[0,     [ Validators.required, Validators.min(0) ]],
+    inStorage:[0, [ Validators.required, Validators.min(0) ]],
   })
 
 
   constructor( private fb: FormBuilder ){}
 
   onSave():void {
-    console.log(this.myForm.value)
+    if(this.myForm.invalid) return;
+    console.log(this.myForm.value);
   }
 
 
