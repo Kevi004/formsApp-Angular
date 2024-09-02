e225b27efe04bb0f18940fa2a1755facd8222e74
Kevin Carrasco <kepler401@gmail.com>
2024-09-02 13:11:48 -0600

    Formulario de registro inicializado

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   // selector: 'app-register-page',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class RegisterPageComponent {
+  public myForm:FormGroup = this.fb.group({
+    name:['', [Validators.required]],
+    email:['', [Validators.required]],
+    userName:['', [Validators.required]],
+    password:['', [Validators.required, Validators.minLength(3)]],
+    password2:['', [Validators.required]],
+  })
+
+  constructor(private fb: FormBuilder){}
+
+  isValidField( field: string ){
+    // Obtener validacion desde un servicio
+  }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+
+  }
 
 }
