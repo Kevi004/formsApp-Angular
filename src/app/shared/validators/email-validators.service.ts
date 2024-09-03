9db69e00d1138ecfacdee57d3ab5dd6c9fc9e719
Kevin Carrasco <kepler401@gmail.com>
2024-09-02 21:48:53 -0600

    Validador de correo agregado

--- src/app/shared/validators/email-validators.service.ts
+++ src/app/shared/validators/email-validators.service.ts
@@ -1,18 +1,33 @@
+
 import { Injectable } from '@angular/core';
-import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { AbstractControl, AsyncValidator, ValidationErrors, Validator } from '@angular/forms';
 import { Observable, of } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class EmailValidators implements AsyncValidator {
 
   constructor() { }
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email = control.value;
+  //   console.log({email});
+
+  //   return of({
+  //     emailTaken: true
+  //   })
+  // }
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email = control.value;
-    console.log({email});
+    const httpCallObservable = new Observable<ValidationErrors | null>((subscriber) => {
+      console.log({email});
+      if(email === 'kebal@gmail.com'){
+        subscriber.next({emailTake: true});
+        subscriber.complete()
+      }
+      subscriber.next(null);
+      subscriber.complete();
 
-    return of({
-      emailTaken: true
-    })
+    });
+    return httpCallObservable;
   }
 
 
